1ee115bc7b6c1f8046e177162598c5f499829e01
Nwachukwu Justin <63079391+thejustinson@users.noreply.github.com>
2024-07-14 22:10:05 +0100

    added link to repository

--- src/components/HeadCard.tsx
+++ src/components/HeadCard.tsx
@@ -6,6 +6,7 @@ const HeadCard = () => {
         <p className="mt-2">Oraculate is a decentralized finance (DeFi) application that aggregates price data from multiple oracle sources, computes an average price, and displays it in a tabular form.</p>
         <p className="mt-1"><span className="font-semibold">Oracles used:</span> Pyth & Diadata</p>
         <p><span className="font-semibold">No of cryptocurrencies used for demo:</span> 20</p>
+        <a href="https://github.com/thejustinson/oraculate.git" target="_blank" className="w-fit"><button className="bg-neutral-900 py-2 px-3 w-fit text-neutral-200 rounded mt-2 active:scale-90 duration-200">Link to repository</button></a>
       </div>
     </div>
   )
